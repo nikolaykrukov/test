680b01d1f5cc04b5d270e66e1c5481715661085e
nikolaykrukov <kolysik00@mail.ru>
2026-07-06 22:46:28 +1000

    Update Calc.java

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
     public int summ(int a, int b){
         int result;
         result = a + b;
-        System.out.println("Сумма " + a + " и " + b + " равно " + result);
+        System.out.println(a + " + " + b + " = " + result);
         return result;
     }
 }
