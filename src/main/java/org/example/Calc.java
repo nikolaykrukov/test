684c87303fe48295554ba99fa040672a1b29b8d2
nikolaykrukov <kolysik00@mail.ru>
2026-07-06 21:17:56 +1000

    Initial commit

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calc {
+
+    public int summ(int a, int b){
+        int result;
+        result = a + b;
+        System.out.println("Сумма " + a + " и " + b + " равно " + result);
+        System.out.printf("Сумма %d и %d равна %d", a, b, result);
+        return result;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    static void main(String[] args) {
+        Calc calculator = new Calc();
+        calculator.summ(1, 10);
+    }
+}
