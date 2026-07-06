8dc0d492f608f9f3b2905e402d877e3ef2615c18
nikolaykrukov <kolysik00@mail.ru>
2026-07-06 23:13:42 +1000

    Update calcTest.java

--- src/test/java/org/example/calcTest.java
+++ src/test/java/org/example/calcTest.java
@@ -23,9 +23,9 @@ class CalcTest {
 //    }
 
 
-    @ParameterizedTest(name = "#{index} - сложение {0} и {1}, ожидаем {2}")
+    @ParameterizedTest(name = "№{index} - сложение {0} и {1}, ожидаем {2}")
     @CsvSource({"10, 2, 12", "-5, 25, 20"})
-    @DisplayName("Проверка суммирования 1")
+    @DisplayName("Проверка суммирования")
     @Tag("param")
     void summ(int a, int b, int expectedResult) {
         Calc calc = new Calc();
@@ -33,9 +33,9 @@ class CalcTest {
         Assertions.assertEquals(expectedResult,result, "Не тот ответ");
     }
 
-    @ParameterizedTest(name = "#{index} - вычитание {0} и {1}, ожидаем {2}")
+    @ParameterizedTest(name = "№{index} - вычитание {0} и {1}, ожидаем {2}")
     @CsvSource({"10, 3, 7", "17, 15, 2"})
-    @DisplayName("Проверка вычитания 1")
+    @DisplayName("Проверка вычитания")
     @Tag("param")
     void sub(int a, int b, int expectedResult) {
         Calc calc = new Calc();
