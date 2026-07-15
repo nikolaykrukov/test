c47622c55c0c6e91a2fd73536accc2f22115f048
nikolaykrukov <kolysik00@mail.ru>
2026-07-15 20:28:47 +1000

    Update Calc.java

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,14 +2,14 @@ package org.example;
 
 public class Calc {
 
-    public int summ(int a, int b){
+    public int summ(int a, int b) {
         int result;
         result = a + b;
         System.out.println(a + " + " + b + " = " + result);
         return result;
     }
 
-    public int sub(int a, int b){
+    public int Minus(int a, int b) {
         int result;
         result = a - b;
         System.out.println(a + " - " + b + " = " + result);
