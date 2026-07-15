cc02c545c77f0d1f0287078a935e56bd591d6924
nikolaykrukov <kolysik00@mail.ru>
2026-07-15 20:35:39 +1000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,7 @@ public class Main {
     static void main(String[] args) {
         Calc calculatorSumm = new Calc();
         calculatorSumm.summ(1, 10);
-
         Calc calculatorMinus = new Calc();
         calculatorMinus.Minus(15, 8);
     }
-
 }
