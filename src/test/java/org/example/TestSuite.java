f1c76ae93c8e27bf8eddf833c354ada899ee3ae3
nikolaykrukov <kolysik00@mail.ru>
2026-07-15 20:28:58 +1000

    Update TestSuite.java

--- src/test/java/org/example/TestSuite.java
+++ src/test/java/org/example/TestSuite.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+
 @Suite
 @SelectPackages("org.example")
 public class TestSuite {
